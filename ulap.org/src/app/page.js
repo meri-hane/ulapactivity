717f9e814ea8a5c66db26361505cc50559fedad4
meri-hane <janecalulang@gmail.com>
2024-10-19 16:23:06 +0800

    changed bg color & added imgs

--- ulap.org/src/app/page.js
+++ ulap.org/src/app/page.js
@@ -2,6 +2,8 @@ import Image from "next/image";
 import i7 from "../app/assets/img/IntelliSeven_Logo 1.png";
 import mapImage from "../app/assets/img/map.png";
 import chatImage from "../app/assets/img/chat.png";
+import googleStore from "../app/assets/img/google_store.png";
+import appStore from "../app/assets/img/apple_store.png";
 
 export default function Home() {
   return (
@@ -16,7 +18,7 @@ export default function Home() {
               height={90}
             />
           </div>
-          <div className="features flex gap-5 text-left font-bold">
+          <div className="features flex gap-5 text-left font-bold text-gray-700">
             <p>Features</p>
             <p>Desktop app</p>
             <p>Privacy & Safety</p>
@@ -25,19 +27,38 @@ export default function Home() {
       </div>
       <div className="flex justify-between items-center">
         <div className="p-20 w-[50%]">
-          <div className="text-[5em] font-semibold leading-snug">
+          <div className="text-[5em] font-semibold leading-snug text-gray-600">
             <p>LawChat</p>
             <p>on your</p>
             <p>pocket.</p>
           </div>
-          <p className="text-gray-500 font-medium">It makes it easy to communicate and ask for help.</p>
+          <p className="text-gray-400 font-medium">It makes it easy to communicate and ask for help.</p>
             <button className="mt-4 px-6 py-2 bg-red-500 text-white font-bold rounded-full hover:bg-red-600">
               Continue as Admin
             </button>
               <a href="#" className="block mt-4 text-blue-500 underline hover:text-blue-700 mx-12">
                 Switch Account
               </a>
+
+          <div className="h-10" /> {/* Add a spacer div with a height of 10 */}
+          <div className="flex gap-4 mt-6">
+          <Image
+              src={appStore} // Replace with your App Store image variable
+              alt="App Store"
+              className="h-[80px] w-[200px] object-contain"
+            />
+            <Image
+              src={googleStore} 
+              alt="Google Store"
+              className="h-[80px] w-[200px] object-contain"
+            />
+            
+          </div>
         </div>
+        
+
+
+
         <div className="relative w-[30%] right-[20%]">
           <div className=" w-[80%] h-[auto] absolute  top-[20%]">
             <Image
@@ -53,7 +74,7 @@ export default function Home() {
             />
         </div>
         </div>
-      
+
       </div>
     </div>
   );
